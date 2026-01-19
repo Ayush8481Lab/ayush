46f13dc37410b099d420a2d2c858d81b718da5bd
Ayush8481Lab <wonder945177@gmail.com>
2026-01-19 21:55:25 +0530

    Update spotifySearchService.js

--- services/spotifySearchService.js
+++ services/spotifySearchService.js
@@ -1,56 +1,47 @@
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
-
+import axios from "axios";
 import { getToken } from "./spotifyAuthService.js";
 
 export async function searchTracks(query) {
-  const { accessToken } = await getToken();
+  try {
+    const accessToken = await getToken();
 
-  const res = await fetch(
-    "https://api-partner.spotify.com/pathfinder/v1/query",
-    {
-      method: "POST",
-      headers: {
-        authorization: `Bearer ${accessToken}`,
-        "content-type": "application/json"
-      },
-      body: JSON.stringify({
-        query: `
-          query Search($q: String!) {
-            search(query: $q, limit: 10) {
-              tracks {
-                items {
-                  uri
-                  name
-                  artists { name }
-                  album {
-                    name
-                    uri
-                    coverArt {
-                      sources { url }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        `,
-        variables: { q: query }
-      })
+    const response = await axios.get(
+      "https://spclient.wg.spotify.com/searchview/v1/search",
+      {
+        params: {
+          q: query,
+          type: "track",
+          limit: 10
+        },
+        headers: {
+          "User-Agent": "Spotify/9.0.34.593 Android",
+          "Accept-Language": "en",
+          "Authorization": `Bearer ${accessToken}`
+        }
+      }
+    );
+
+    const items =
+      response.data?.tracks?.items ||
+      response.data?.sections?.[0]?.items;
+
+    if (!items || !items.length) {
+      throw new Error("No search results");
     }
-  );
 
-  const json = await res.json();
-
-  const items = json?.data?.search?.tracks?.items;
-  if (!items) throw new Error("No results");
-
-  return items.map(t => ({
-    song: t.name,
-    artist: t.artists.map(a => a.name).join(", "),
-    album: t.album.name,
-    albumUrl: `https://open.spotify.com/album/${t.album.uri.split(":")[2]}`,
-    songUrl: `https://open.spotify.com/track/${t.uri.split(":")[2]}`,
-    image: t.album.coverArt.sources[0]?.url || null
-  }));
-}
+    return items.map(item => {
+      const track = item.track;
+      return {
+        song: track.name,
+        artist: track.artists.map(a => a.name).join(", "),
+        album: track.album.name,
+        albumUrl: `https://open.spotify.com/album/${track.album.uri.split(":")[2]}`,
+        songUrl: `https://open.spotify.com/track/${track.uri.split(":")[2]}`,
+        image: track.album.coverArt.sources[0]?.url || null
+      };
+    });
+  } catch (error) {
+    console.error("Spotify search error:", error?.response?.data || error);
+    throw error;
+  }
+                     }
